084829b7b474d360083ba8d1dbf612f1e2b40792
Connor-Yerxa <cyerxa@unb.ca>
2025-11-25 11:31:42 -0400

    Add menus.h

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "menus.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
