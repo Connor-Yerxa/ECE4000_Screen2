2bafada72d9a914f5e404b80f9dd5bdd7bfbe639
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2025-11-26 08:57:42 -0400

    Update menus.h

--- Core/Inc/menus.h
+++ Core/Inc/menus.h
@@ -1,3 +1,18 @@
 /* menus for the display interface */
 
 
+/* Start on main interface, have text boxes appear, with text added separately, potentially as variables */
+
+/*Types of text: 0-9, confirm or cancel, Main menu, Settings */
+
+/*
+
+
+
+
+
+
+
+*/
+
+
