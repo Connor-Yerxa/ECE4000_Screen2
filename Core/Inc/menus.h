52bd97b24b736883a3a39487099c586ac0ed404e
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2025-11-26 09:07:43 -0400

    Update menus.h

--- Core/Inc/menus.h
+++ Core/Inc/menus.h
@@ -6,13 +6,47 @@
 /*Types of text: 0-9, confirm or cancel, Main menu, Settings */
 
 /*
+// Screen variable
+int screen = 0;
+char text[10][20];
 
+// constant loop
+  while(1) {
+      //Main menu 0-99
+      if (screen < 100){
+          text = {"New Test", "Calibration", "Settings", "Data"}
+      }
+      //testing
+      else if (screen < 200){
 
+      }
+      
+      //Calibration
+      else if (screen < 300){
 
+      }   
+      
+      //Settings
+      else if (screen < 400){
+    
+      }
+
+      //Data
+      else if (screen < 500){
+
+      
+      }
+      //Out of range
+      else{
+          printf("Error: outside of range")
+      }
+
+}
 
 
 
 
 */
+
 
 
