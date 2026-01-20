cdb4ea3ed4a6232df00a0e12352c60069563ee48
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-20 15:07:49 -0400

    Create readButtons.c
    basic structure, needs updating once we have all 6 buttons

---
+++ Core/Src/readButtons.c
@@ -0,0 +1,10 @@
+/* Reads the button stats, returns 0-6 depending on which are pressed */
+//Ignores multiple buttons, just returns the first
+
+double readButtons(){
+  //Read in buttons from pin activity
+  buttonsPressed = 0 //set this up so that it updates to the first button pressed
+  return buttonsPressed
+}
+
+//Setup after buttons are added! - PCB needed
