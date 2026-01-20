64ac24360133cec80a250bd3500d896f85dbb6e0
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-20 15:44:12 -0400

    Update numberSelect.c

--- Core/Src/numberSelect.c
+++ Core/Src/numberSelect.c
@@ -2,20 +2,9 @@
 #include readButtons.h
 
 
-int length = 0;
 int button = 0;
 
-double numberSelect(double selectionID){
-  if (selectionID = 1){ //Type is temperature
-    length = 3;
-  }
-  if (selectionID = 2){ //Type is Time
-
-  }
-  if (selectionID = 3){ //Type is other
-
-  }
-
+double numberSelect(double selectionID, int length){
   while (length > 0){
     button = readButtons();
     
