6d116c39c9d7d48cf274ebbf62612255cef1b566
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-20 15:01:59 -0400

    Update menus.c

--- Core/Src/menus.c
+++ Core/Src/menus.c
@@ -1,70 +1,91 @@
-//Code for navigating menus
 /* menus for the display interface */
-
-
 /* Start on main interface, have text boxes appear, with text added separately, potentially as variables */
-
-/*Types of text: 0-9, confirm or cancel, Main menu, Settings */
 
 /*
 // Screen variable
-int screen = 0;
+int screen = 0; // sets where the screen starts
 char text[6][20]; //textbox filler (6 buttons, 20char max)
 char display[1][50]; //Screen display
 
-// constant loop
+//Expected inputs: Buttons 1-6
+//Outputs: Display
+//Screen starts at "stage" 0, and alters in an else-if statement until it reaches the section that needs to update.
+
+
   while(1) {
       //Main menu 0-99
       if (screen < 100){
           display = {"Menu"}
           text = {"New Test", "Calibration", "Settings", "Data", "", ""}
+          //READBUTTONFUNCTION
       }
       //Testing
       else if (screen < 200){
         display = {"Select testing limit(s)"}
         both = 0
-        text = {"Time","Temperature","Both","Default","","","","","",""}
+        text = {"Time","Temperature","Both","Default","",""} //'Both' will do 110 and 111 in order, default will reset.
+        //READBUTTONFUNCTION
         
-        //'Both' will do 110 and 111 in order
+        
         if (screen = 110){ //Time select
          display = "Select time limit (max) 10 minutes"
-         //NUMBER SELECT FUNCTION
+         //READBUTTONFUNCTION
          if (both = 1){  //Active if both was selected
             screen = 111;
          }
         }
+        
         if (screen = 111){ //Temperature Select
          display = "Select temperature delta limit (max 10 degrees)"
-          //NUMBER SELECT FUNCTION
+          //READBUTTONFUNCTION
         }          
         if (screen = 120){ // Test running, runs until done or alternative reached
          display = "Test underway"
          text = {"Cancel","","","","",""}
+        //READBUTTONFUNCTION??? OR JUST WAIT
         }
       }
       
       //Calibration
       else if (screen < 300){
         display = {"Run calibration?"}
-        text = {"Confirm","Cancel","Set default","","","","","","",""}
-        if (screen = 210)
+        text = {"Confirm","Set default","","","","Cancel"}
+        //READBUTTONFUNCTION
+        
+        if (screen = 210){
           display = {"Select calibration material"}
-          text = {"A","B","Cancel","","","","","","",""}
-        if (screen = 220)
+          text = {"Material-A","material-B","","","","Cancel"}
+          //READBUTTONFUNCTION
+        }
+        
+        if (screen = 220){
           display = {"Running calibration."}
-          text = {"Cancel","","","","","","","","",""}
-        if (screen = 230)
+          text = {"Cancel","","","","",""}
+          //READBUTTONFUNCTION
+        }
+        if (screen = 230){
           display = {"Calibration sucessful"}
-          text = {"Return","","","","","","","","",""}
+          text = {"Return","","","","",""}
+          //READBUTTONFUNCTION
+        }
       }   
             
       //Settings
       else if (screen < 400){
         display = {"Settings"}
-        text = {"Brightness","Sample time","Sample duration","","","","","","","Back"}
-        if (screen = 310){
+        text = {"Brightness","Default Sample temp cuttoff","Default Sample duration","","","Back"}
+        
+        if (screen = 310){//Brightness
           display = "Brighness 1-10"
-          text = {"1","2","3","4","5","6","7","8","9","10"}
+          //READBUTTONFUNCTION
+        }
+                if (screen = 310){//Sample Temp
+          display = "Temperature change"
+          //READBUTTONFUNCTION
+        }
+                if (screen = 310){//Sample Duration
+          display = "Sample Duration"
+          //READBUTTONFUNCTION
         }
       }
         
