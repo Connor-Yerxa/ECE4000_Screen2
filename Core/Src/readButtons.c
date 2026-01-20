64646af5aec27c2b6cb72d2a2443d66e98200323
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-20 15:17:33 -0400

    Update readButtons.c

--- Core/Src/readButtons.c
+++ Core/Src/readButtons.c
@@ -1,9 +1,39 @@
 /* Reads the button stats, returns 0-6 depending on which are pressed */
 //Ignores multiple buttons, just returns the first
 
+buttonPressed = 0
+button1 = false
+button2 = false
+button3 = false
+button4 = false
+button5 = false
+button6 = false
+
 double readButtons(){
-  //Read in buttons from pin activity
-  buttonsPressed = 0 //set this up so that it updates to the first button pressed
+  while(buttonPressed =  0){
+  //Read in buttons from pin activity as booleans, if any are pressed change buttonX
+
+
+  //Write buttonPressed  
+    if (button1 = true){
+      buttonPressed = 1;
+    }
+    else if (button2 = true){
+      buttonPressed = 2;
+    }
+    else if (button3 = true){
+      buttonPressed = 3;
+    }
+    else if (button4 = true){
+      buttonPressed = 4;
+    }
+    else if (button5 = true){
+      buttonPressed = 5;
+    }
+    else if (button6 = true){
+      buttonPressed = 6;
+    }
+  }
   return buttonsPressed
 }
 
