324ba80c2f420a4777b6e864b36e43bfd6c9efbd
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-20 15:41:45 -0400

    Update displayText.c

--- Core/Src/displayText.c
+++ Core/Src/displayText.c
@@ -1,1 +1,10 @@
+double displayText(double ID, double extra){
+  //reads in ID, shows text based on ID or screen number
+  //extra is to read in other values to show
 
+  
+  if (ID = 1){//read numbers    
+  }
+
+  return
+}
