a9b11ad92f5a4338a35f7be3564a3b93d5e124d0
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-20 15:36:36 -0400

    Create numberSelect.c

---
+++ Core/Src/numberSelect.c
@@ -0,0 +1,30 @@
+//Allows selection of numbers
+#include readButtons.h
+
+
+int length = 0;
+int button = 0;
+
+double numberSelect(double selectionID){
+  if (selectionID = 1){ //Type is temperature
+    length = 3;
+  }
+  if (selectionID = 2){ //Type is Time
+
+  }
+  if (selectionID = 3){ //Type is other
+
+  }
+
+  while (length > 0){
+    button = readButtons();
+    
+
+    length--;
+  }
+
+
+    //LIMITS
+
+  
+}
