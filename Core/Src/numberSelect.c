024c45ffbe6fc8ac98c7e8cec3c0fbd5d6c7877a
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-21 06:37:34 -0400

    Update numberSelect.c

--- Core/Src/numberSelect.c
+++ Core/Src/numberSelect.c
@@ -1,14 +1,16 @@
 //Allows selection of numbers
 #include readButtons.h
-
+#include displayText.h
 
 int button = 0;
+int number = 0;
+int extra = 0;
 
 double numberSelect(double selectionID, int length){
   while (length > 0){
     button = readButtons();
     
-
+    displayText()
     length--;
   }
 
