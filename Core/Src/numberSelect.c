6166edd0929f41daba029ae84c688b96219b63b5
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-21 06:48:14 -0400

    Update numberSelect.c

--- Core/Src/numberSelect.c
+++ Core/Src/numberSelect.c
@@ -1,21 +1,34 @@
 //Allows selection of numbers
 #include readButtons.h
 #include displayText.h
+#include math.h
 
 int button = 0;
 int number = 0;
 int extra = 0;
+int multiplier = 0;
 
 double numberSelect(double selectionID, int length){
   while (length > 0){
     button = readButtons();
-    
-    displayText()
-    length--;
+    if (button = 4){ //increase the number
+      number++;
+    }
+    if (button = 5){
+      multiplier = pow(10, length); //shifts it over
+      extra = extra + muliplier*number; //add multiplied value
+      number = 0;  //reset number
+      length--; //move to the next number
+    }
+    if button = 6){ //decrease the number
+      number--;
+    }
+    displayText(); //Display the numbers and buttons
   }
-
-
-    //LIMITS based on ID
-
   
+    //LIMITS & Checks based on ID (Add more to this!)
+  if (extra > 1000){
+    extra = 1000;
+  }
+  return extra
 }
