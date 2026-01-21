3370f3efeef5ef22d902b49e975e3adc7efe4dae
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-21 07:10:15 -0400

    Update displayText.c

--- Core/Src/displayText.c
+++ Core/Src/displayText.c
@@ -9,8 +9,50 @@ double displayText(double ID, double extra){
   if (ID = 0){//main menu
     display = {"Menu"}
   }
-
-
+  if (ID = 100){
+      display = {"Select testing limit(s)"}
+      text = {"Time","Temperature","Both","Default","",""} //'Both' will do 110 and 111 in order, default will reset.
+  } 
+  if (ID = 110){
+      display = {"Select time limit (max) 10 minutes"}
+      text = extra;
+  }
+  if (ID = 111){
+      display = {"Select temperature delta limit (max 10 degrees)"}
+      text = extra;
+  }
+  if (ID = 120){
+      display = {"Test underway"}
+  }
+  if (ID = 200){
+      display = {"Run calibration?"}
+  }
+  if (ID = 210){
+      display = {"Select calibration material"}
+  }
+  if (ID = 220){
+      display = {"Running calibration."}
+  }
+  if (ID = 230){
+      display = {"Calibration sucessful"}
+  }
+  if (ID = 300){
+      display = {"Settings"}
+  }
+  if (ID = 310){
+      display = {"Brighness 1-10"}
+  }
+  if (ID = 320){
+      display = {"Temperature change"}
+  }
+  if (ID = 330){
+      display = {"Sample Duration"}
+  }
+  if (ID = 400){
+      display = {"Data"}
+  }
+              
+              
 
   //OUTPUT TO BOARD
 
