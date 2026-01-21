3f24e4ef5080c0cf7d294317e04debbb5764e707
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-01-21 07:02:13 -0400

    Update displayText.c

--- Core/Src/displayText.c
+++ Core/Src/displayText.c
@@ -1,10 +1,13 @@
+char text[6][20]; //textbox filler (6 buttons, 20char max)
+char display[1][50]; //Screen display
+
 double displayText(double ID, double extra){
   //reads in ID, shows text based on ID or screen number
   //extra is to read in other values to show
 
   
   if (ID = 0){//main menu
-    
+    display = {"Menu"}
   }
 
 
