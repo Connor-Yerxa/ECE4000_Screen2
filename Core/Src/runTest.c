56bf52efb635ee02458beec47e4e73a28d365e6c
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-02-12 13:38:16 -0400

    Update runTest.c

--- Core/Src/runTest.c
+++ Core/Src/runTest.c
@@ -3,3 +3,35 @@
 
 //Take in data related to the input values
 //deltaTemp, deltaTime
+
+//Test should - take in values at each time sample, and store it somehow. from there it should be able to plot it after it finishes
+//Needs to run for deltaTemp or deltaTime
+
+
+//MATRIX SETUP MAY NEED TO BE A POINTER BASED ONE TO ALLOW VARIABLE SIZE
+/*
+
+int running = 1;
+double testTemp;
+double startingTemp;
+
+//Startup measurements
+  startingTemp = getTemp();
+  
+
+while(running = 1){
+  //measure test data
+  //log test data
+  //verify it hasnt left safe boundries
+
+  testDeltaTemp = startingTemp - testTemp;
+  if(testDeltaTemp >= deltaTemp){ //check if the test temp is outside the range
+      running = 0; //End the test
+  }
+  if(testTime >= deltaTime){ //check if the test time is outside the range
+        running = 0; //End the test
+  }  
+}
+
+
+*/
