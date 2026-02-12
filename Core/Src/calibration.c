e69bb0b289f0fa5f0710f6b733ee5e89a1d45c91
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-02-12 13:40:55 -0400

    Update calibration.c

--- Core/Src/calibration.c
+++ Core/Src/calibration.c
@@ -1,1 +1,11 @@
 //calibration
+
+
+
+/*
+1. Take in material type
+2. measure the k value
+3. find the delta between the k value and theoretical value
+4. return a difference between the two values -- main menu should then update the offset for tests
+
+*/
