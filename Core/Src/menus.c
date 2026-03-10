3ba7e8fc2d908362697c7cea154354967e2b9187
HolmesNathan <135063258+HolmesNathan@users.noreply.github.com>
2026-03-10 14:39:21 -0300

    Update menus.c

--- Core/Src/menus.c
+++ Core/Src/menus.c
@@ -1,10 +1,10 @@
 /* menus for the display interface */
 /* Start on main interface, have text boxes appear, with text added separately, potentially as variables */
 
-#include menus.h //links to .h
-#include read_Buttons.h //Allows the reading of buttons
-#include selectNumber.h //Allows for the selection of a number 0-9
-#include displayText.h //Lets the screen display the right text
+#include "menus.h" //links to .h
+#include "read_Buttons.h" //Allows the reading of buttons
+#include "selectNumber.h" //Allows for the selection of a number 0-9
+#include "displayText.h" //Lets the screen display the right text
   
 // variable setup
 int screen = 99; // sets where the screen starts
